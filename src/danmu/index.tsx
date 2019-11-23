711b91509df63c23489ad2dbdf716b762331b80f
qinvz <sun.houtao@outlook.com>
2019-11-23 15:18:44 +0800

    弹幕方案一

--- src/danmu/index.tsx
+++ src/danmu/index.tsx
@@ -1,15 +1,22 @@
+/*
+ * 方案一:直接在给数组添加left, top属性, 然后使用帧动画更新数组;
+ * 缺点: 动画效果卡顿, 每次更新后的渲染时间在200ms, 弹幕一顿一顿的;
+ * 优点:使用简单, 可控制动画的暂停启动, 无需关注时间, 只需维护数组;
+ * 反思:可能是组件依赖过多, 导致每次render时间过长, 下一步尝试将位移动画提成组件, 减少依赖, 提高动画流畅度
+ */
+
 import React, { useEffect, useRef, useContext, useState, useLayoutEffect } from 'react';
 import { PlayerContext } from '../model';
 import styled from 'styled-components';
-import { fontArr, areaArr, opacityArr } from '../utils/utils';
+import { fontArr, areaArr, opacityArr, getFontLength } from '../utils/utils';
 import fetch from '../utils/request';
-import Text from './text';
 
 interface props {
   mode: string;
   size: number;
   area: number;
   opacity: number;
+  width: number;
 }
 
 const Wrapper = styled.div`
@@ -28,6 +35,14 @@ const Wrapper = styled.div`
     font-size: ${(props: props) => fontArr[props.mode][props.size]}px;
     line-height: ${(props: props) => fontArr[props.mode][props.size] + 4}px;
     opacity: ${(props: props) => opacityArr[props.mode][props.opacity]};
+
+    .danmu {
+      position: absolute;
+      display: inline-block;
+      right: ${(props: props) => props.width}px;
+      white-space: nowrap;
+      transition: all 200 linear;
+    }
   }
 `;
 
@@ -42,13 +57,16 @@ const reactComponent: React.FC<PropsType> = props => {
   if (!danmuShow) return <></>;
 
   const danmuRef: React.RefObject<T> = useRef(null);
-  const countRef: React.RefObject<T> = useRef({});
+  const storeRef: React.RefObject<T> = useRef({});
+
+  if (!storeRef.current.top) {
+    storeRef.current.top = [];
+  }
 
   const [list, setList] = useState<Array<any>>([]);
   const [show, setShow] = useState<Array<any>>([]);
   const [width, setWidth] = useState<number>(0);
-  const [height, setHeight] = useState<number>(0);
-  let [left, setLeft] = useState<number>(0);
+  const [total, setTotal] = useState(0);
 
   const initData = async (target: string) => {
     const data = await fetch(target).then(res => res.json());
@@ -56,14 +74,93 @@ const reactComponent: React.FC<PropsType> = props => {
   };
 
   const filterData = (list: Array<any>) => {
-    const data = list
-      .filter(item => item.time <= current + 0.5 && item.time >= current - 0.5)
-      .filter(item => !show.some(ele => ele._id === item._id));
-    show.push(...data);
+    list
+      .filter(
+        item =>
+          item.time < current + 0.5 &&
+          item.time > current - 0.5 &&
+          !show.some(ele => ele._id === item._id),
+      )
+      .map(item => {
+        draw(item);
+      });
+  };
+
+  const draw = (value: any) => {
+    const result = getEmptyDanmuTop();
+    const selfWidth = (getFontLength(value.text) * fontArr[mode][danmuFont]) / 2;
+    const preLeft = width + selfWidth;
+    show.push({
+      ...value,
+      left: result.left < width ? preLeft : result.left + 30 + selfWidth,
+      top: result.top,
+    });
     setShow(show);
   };
 
-  const draw = (list: Array<string>) => {};
+  const getEmptyDanmuTop = () => {
+    show.map(item => {
+      const left = item.left;
+      const top = item.top;
+      storeRef.current.top[top] = {
+        left,
+        top,
+      };
+    });
+
+    let result = [...storeRef.current.top].sort((a, b) => a.left - b.left);
+    const lessDanmu = result
+      .filter(item => item.top < total / 2 && item.top > 0 && Math.abs(width - item.left) < 100)
+      .sort((a, b) => a.top - b.top);
+    if (lessDanmu.length > 0) {
+      result = lessDanmu;
+    }
+
+    return result[0];
+  };
+
+  // const cancel = (value: string) => {
+  //   let index;
+  //   show.some((item, index) => {
+  //     if (item._id === value) {
+  //       index = index;
+  //       return true;
+  //     }
+  //   });
+  //   show.splice(index, 1);
+  //   setShow(show);
+  // };
+
+  const start = () => {
+    show.map(item => {
+      item.left -= 0.5;
+    });
+
+    for (let index = 0; index < show.length; index++) {
+      const element = show[index];
+      if (element.left < 0) {
+        show.splice(index, 1);
+        index--;
+      }
+    }
+
+    setShow(show);
+    storeRef.current.play = requestAnimationFrame(start);
+  };
+
+  const stop = () => {
+    if (storeRef.current.play) {
+      cancelAnimationFrame(storeRef.current.play);
+    }
+  };
+
+  useEffect(() => {
+    if (play) {
+      start();
+    } else {
+      stop();
+    }
+  }, [play]);
 
   useEffect(() => {
     initData(danmu);
@@ -73,30 +170,53 @@ const reactComponent: React.FC<PropsType> = props => {
     filterData(list);
   }, [current]);
 
-  useEffect(() => {
-    draw(show);
-  }, [show]);
+  // useEffect(() => {
+  //   draw(show);
+  // }, [show]);
 
   useEffect(() => {}, []);
 
   useLayoutEffect(() => {
     setWidth(danmuRef.current.clientWidth);
-    setHeight(danmuRef.current.clientHeight);
+    const newTotal =
+      Math.floor((danmuRef.current.clientHeight - 40) / fontArr[mode][danmuFont] + 4) - 1;
+
+    if (newTotal === total) return;
+    [...Array(newTotal)].map((item, index) => {
+      if (!storeRef.current.top[index]) {
+        storeRef.current.top[index] = {
+          left: -1,
+          top: index,
+        };
+      }
+    });
+    storeRef.current.top.length = newTotal;
+
+    setTotal(newTotal);
   });
 
   return (
-    <Wrapper ref={danmuRef} size={danmuFont} mode={mode} area={danmuArea} opacity={danmuOpacity}>
+    <Wrapper
+      ref={danmuRef}
+      size={danmuFont}
+      mode={mode}
+      area={danmuArea}
+      opacity={danmuOpacity}
+      width={width}
+      play={play}
+    >
       <div className="con">
         {show.map(item => (
-          <Text
+          <div
+            className="danmu"
             key={item._id}
-            text={item.text}
-            left={left}
-            top={0}
-            size={fontArr[mode][danmuFont]}
-            width={width}
-            play={play}
-          ></Text>
+            style={{
+              transform: `translateX(${item.left}px)`,
+              top: item.top * (fontArr[mode][danmuFont] + 4),
+            }}
+          >
+            {item.text}
+          </div>
         ))}
       </div>
     </Wrapper>
